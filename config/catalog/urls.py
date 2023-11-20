ff6bc5c6e2e33c61ec3b6030e8935d8aa2bbc0c8
11Weldon <jiltsov.nikita2017@yandex.ru>
2023-11-20 23:05:18 +0300

    cart
    Signed-off-by: 11Weldon <jiltsov.nikita2017@yandex.ru>

--- config/catalog/urls.py
+++ config/catalog/urls.py
@@ -2,12 +2,10 @@ from rest_framework import routers
 from .api import ProductViewSet
 
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 
 from . import views
+from .views import CartAPI
 
 
 router = routers.DefaultRouter()
@@ -15,11 +13,10 @@ router.register('api/catalog', ProductViewSet, 'product')
 
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/home/', views.HomeView.as_view(), name ='home'),
+    path('api/logout/', views.LogoutView.as_view(), name ='logout'),
+    path('api/cart', CartAPI.as_view(), name='cart'),
 
-    path('home/', views.HomeView.as_view(), name ='home'),
-    path('logout/', views.LogoutView.as_view(), name ='logout')
 ]
 
 # Распаковываем URL-пути из router.urls и добавляем их к urlpatterns
--- config/catalog/views.py
+++ config/catalog/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
+from .service import Cart
 
 
 class HomeView(APIView):
@@ -26,3 +27,39 @@ class LogoutView(APIView):
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class CartAPI(APIView):
+    """
+    Single API to handle cart operations
+    """
+    def get(self, request, format=None):
+        cart = Cart(request)
+
+        return Response(
+            {"data": list(cart.__iter__()),
+            "cart_total_price": cart.get_total_price()},
+            status=status.HTTP_200_OK
+            )
+
+    def post(self, request, **kwargs):
+        cart = Cart(request)
+
+        if "remove" in request.data:
+            product = request.data["product"]
+            cart.remove(product)
+
+        elif "clear" in request.data:
+            cart.clear()
+
+        else:
+            product = request.data
+            cart.add(
+                    product=product["product"],
+                    quantity=product["quantity"],
+                    overide_quantity=product["overide_quantity"] if "overide_quantity" in product else False
+                )
+
+        return Response(
+            {"message": "cart updated"},
+            status=status.HTTP_202_ACCEPTED)
